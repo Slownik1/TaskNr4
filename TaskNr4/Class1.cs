ae7b2d61275e2fdd9696455685de2c2fc7c2cd6e
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-04-01 20:35:06 +0200

    Add files via upload

---
+++ TaskNr4/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskNr4
+{
+    public struct Zespolone
+    {
+
+        public int real;
+        public int imaginary;
+
+        Zespolone(int real, int imaginary)
+        {
+
+            this.real = real;
+            this.imaginary = imaginary;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{real}+{imaginary}i";
+        }
+
+        public override bool Equals(Zespolone other)
+        {
+            if (other == null)
+                return false;
+
+            Zespolone x = (Zespolone)other;
+
+            return other.imaginary == imaginary && other.real == real;
+        }
+
+    }
+}
